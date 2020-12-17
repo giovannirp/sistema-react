329e327e97269b1c356de0a414b34d7a87e65f39
Giovanni Ribeiro Pinto <giovanni.ribeiro@vertem.com>
2020-12-17 20:42:40 -0300

    feat: adicionando use interval

--- sistema-react/src/Componentes/Banner/BannerHome.js
+++ sistema-react/src/Componentes/Banner/BannerHome.js
@@ -1,10 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import useInterval from '@use-it/interval';
 import BannerImg from "../../assets/banner.jpg";
 
 function BannerHome() {
+  const words = ['Experiência em viagem', 'Novas Viagem', 'tudo te']
+  const [currentWolrd, setCurrentWolrd] = useState(0)
+
+  const logicWolrd = () => (currentWolrd + 1 < words.length ? currentWolrd + 1 : 0) 
+
+  useInterval(() => {
+    setCurrentWolrd(logicWolrd())
+  }, 2000)
+
   return (
     <div>
       <img src={BannerImg} />
+      <div>
+       {words[currentWolrd]}
+        <br />
+      </div>
     </div>
   );
 }
