07eb99b936a0f42308df2951318d129926660a88
Giovanni Ribeiro Pinto <giovanni.ribeiro@vertem.com>
2020-12-18 19:32:30 -0300

    feat: adicionando novos textos

--- sistema-react/src/Componentes/Banner/BannerHome.js
+++ sistema-react/src/Componentes/Banner/BannerHome.js
@@ -1,23 +1,22 @@
 import React, { Fragment, useEffect, useState } from "react";
-import useInterval from '@use-it/interval';
+import useInterval from "@use-it/interval";
 import BannerImg from "../../assets/banner.jpg";
 
 function BannerHome() {
-  const words = ['Experiência em viagem', 'Novas Viagem', 'tudo te']
-  const [currentWolrd, setCurrentWolrd] = useState(0)
-
-  const logicWolrd = () => (currentWolrd + 1 < words.length ? currentWolrd + 1 : 0) 
+  const words = ["Experiência em viagem", "Novas Viagem", "Boas Promoões"];
+  const [currentWolrd, setCurrentWolrd] = useState(0);
+  const logicWolrd = () =>
+    currentWolrd + 1 < words.length ? currentWolrd + 1 : 0;
 
   useInterval(() => {
     setCurrentWolrd(logicWolrd())
   }, 2000)
 
   return (
-    <div>
-      <img src={BannerImg} />
-      <div>
-       {words[currentWolrd]}
-        <br />
+    <div class='container-banner'>
+      <img src={BannerImg} alt="Banner" />
+      <div className='text-animate'>
+        {words[currentWolrd]}
       </div>
     </div>
   );
